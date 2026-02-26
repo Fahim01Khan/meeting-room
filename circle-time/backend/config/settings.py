b9a01f887e076af7eeaff6c3ab52df718b306c93
Patel Mohamad Fahim Khan <fk3076521@gmail.com>
2026-02-26 14:58:04 +0200

    Fix: .env loading, console email backend, real admin user, accept invite page (Week 3 Day 2)

--- circle-time/backend/config/settings.py
+++ circle-time/backend/config/settings.py
@@ -157,7 +157,10 @@ PSEUDONYMIZE_AFTER_DAYS = int(os.environ.get("PSEUDONYMIZE_AFTER_DAYS", "30"))
 # ---------------------------------------------------------------------------
 # Email (SendGrid SMTP relay)
 # ---------------------------------------------------------------------------
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend",
+)
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
